aa1604dc21292415768211bd7982eaa2d3ac552e
rehman <rehmankhan345890@gmail.com>
2026-03-09 13:33:12 +0530

    updated preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+
 
 def build_preprocessor(X):
 
@@ -9,10 +11,12 @@ def build_preprocessor(X):
     categorical_features = X.select_dtypes(include=["object", "category"]).columns
 
     numeric_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="mean")),
         ("scaler", StandardScaler())
     ])
 
     categorical_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="most_frequent")),
         ("onehot", OneHotEncoder(handle_unknown="ignore"))
     ])
 
